1b2568586a003473c010d565a26534630af4c4c6
bh1990 <56873720+bh1990@users.noreply.github.com>
2019-10-25 21:28:37 +0700

    Add files via upload

---
+++ tugas2/src/tugas2/Tugas2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2;
+
+/**
+ *
+ * @author user
+ */
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // "f
+        for (int b = 0; b < 10; b++) {
+            System.out.println(b);
+        }
+    }
+    
+}
